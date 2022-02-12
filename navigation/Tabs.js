a60d4d5e38e75d8111c494d56dc316f37185e972
Hong <gg9297@gmail.com>
2022-02-12 14:45:25 +0900

    Add file

--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -7,7 +7,7 @@ import Search from "../screens/Search";
 const Tab = createBottomTabNavigator();
 
 const Tabs = () => (
-  <Tab.Navigator>
+  <Tab.Navigator initialRouteName="Search">
     <Tab.Screen name="Movies" component={Movies} />
     <Tab.Screen name="Tv" component={Tv} />
     <Tab.Screen name="Search" component={Search} />
